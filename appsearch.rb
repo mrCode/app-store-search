2891e5cddbddf3c88dbd68385f635e1c18f3cf52
Basem Aljedai <Basem@Basems-MacBook-Pro.local>
2014-12-12 23:01:52 +0300

    Add logic to download apps icons in tmp/icons folder

--- appsearch.rb
+++ appsearch.rb
@@ -3,6 +3,8 @@ require 'json'
 
 require 'sinatra'
 
+require 'open-uri' 
+
 require 'itunes-search-api'
 require 'market_bot'
 
@@ -20,7 +22,9 @@ post '/search' do
   unix_time = Time.now.to_i
   
   files = market_names.map { |mn| "tmp/#{query}_#{mn}_#{unix_time}.csv" }
-  
+    
+  artwork = []
+
   [itunes, play].each_with_index do |store_results, idx|
     CSV.open(files[idx], "w") do |csv|
       store_results.each_with_index do |hash, idx|
@@ -28,15 +32,37 @@ post '/search' do
           csv << hash.keys
         else
           csv << hash.values
+          artwork << hash["artworkUrl60"]
         end
       end
     end
   end
   
+
+  puts artwork.compact!
+  puts artwork.count
+  iconsDownloader(artwork)
+
   files.map! {|file| file.gsub('tmp/', 'file/') }
   files.to_json
 end
 
 get '/file/*' do |file|
   send_file "tmp/" + file
+end
+
+
+def iconsDownloader(artwork=[])
+   if artwork.count == 0 
+     puts "Error Empty artwork!!"
+   else
+     artwork.each_with_index do |name, idx|
+       puts "File to be downloaded: #{name}"
+       File.open("tmp/icons/#{idx}.png", "wb") do |saved_file|
+        open(name,"rb") do |read_file|
+          saved_file.write(read_file.read)
+        end
+      end
+     end
+   end
 end
